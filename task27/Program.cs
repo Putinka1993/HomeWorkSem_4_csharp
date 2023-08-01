2b97556bbcdaddd47f895df160648e47d452cbdc
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-08-01 11:42:32 +0300

    task27 ready

---
+++ task27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("enter the digit: ");
+int num = int.Parse(Console.ReadLine());
+
+string numS = num.ToString();
+
+int length = numS.Length;
+int sum = 0;
+int sumNum(string numS, int length){
+    for (int i = 0; i < length; i++){
+        sum += int.Parse(numS[i].ToString());
+    }
+    return sum;
+}
+int res = sumNum(numS, length);
+Console.WriteLine(res);
