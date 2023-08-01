fbd4f3c0c0a1eddd8286a921c45bca4f1a292d55
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-08-01 06:33:03 +0300

    task25 ready

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("enter the number A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the number B: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int Exponentiate(int a, int b) {
+    int m = a;
+    for (int i = 1; i < b; i++)
+        a *= m; 
+    return a;
+}
+
+int res = Convert.ToInt32(Exponentiate(a, b));
+Console.WriteLine($"Степень числа {a} и {b} равна {res}");
