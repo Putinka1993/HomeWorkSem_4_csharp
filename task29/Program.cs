8f3f632dedbdbd4387fd00c3a9c3c07e2551b036
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-08-02 11:51:37 +0300

    task ready29

---
+++ task29/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+Console.WriteLine("Введите желаемую длину массива: ");
+int length = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите диапозон чисел от и до");
+Console.WriteLine("Введите число ОТ скольки: ");
+int minValue = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число ДО скольки: ");
+int maxValue = int.Parse(Console.ReadLine());
+
+
+int[] array_create(int length, int minValue, int maxValue) {
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++){
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int[] res = array_create(length, minValue, maxValue);
+
+Console.WriteLine($"[{String.Join(", ", res)}]");
